61237a82d2bf07a775c24e72e32380a4119c00c1
Johnny Cage <thespammingbott@gmail.com>
2026-04-27 14:20:51 -0700

    parse express form inputs & import and use indexRouter

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const indexRouter = require("./routers/indexRouter")
 const app = express()
 const PORT = 3000
+
+app.use(express.urlencoded({ extended: false }))
+app.use("/", indexRouter)
 
 app.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`)
