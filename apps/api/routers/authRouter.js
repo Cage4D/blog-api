954bd0f1a2dd19df3d52c361164e106a9df55612
Johnny Cage <thespammingbott@gmail.com>
2026-04-28 00:54:36 -0700

    implement: authRouter

---
+++ apps/api/routers/authRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require("express")
+const authRouter = Router()
+
+authRouter.get("/auth/signup", (req, res) => {})
+authRouter.post("/auth/login", (req, res) => {})
+authRouter.post("/auth/logout", (req, res) => {})
--- apps/api/routers/indexRouter.js
+++ apps/api/routers/indexRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require("express")
 const indexRouter = Router()
 
-indexRouter.get("/", (req, res) => {})
+indexRouter.get("/", (req, res) => {
+    res.send("listening on port 3000")
+})
 indexRouter.post("/", (req, res) => {})
 
 module.exports = indexRouter;
