09b1e0567f5c2f2076e4cf311791a37fcb0fefee
Johnny Cage <thespammingbott@gmail.com>
2026-04-28 01:18:13 -0700

    implement authController

---
+++ apps/api/controllers/authController.js
@@ -0,0 +1,19 @@
+const prisma = require("../../../prisma/client")
+
+async function signup(req, res) {
+    res.json("")
+}
+
+async function login(req, res) {
+    res.json("")
+}
+
+async function logout(req, res) {
+    res.json("")
+}
+
+module.exports = {
+    signup,
+    login,
+    logout,
+}
