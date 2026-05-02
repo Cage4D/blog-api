7a569fd035d305daba72f3b6d0411a776b07a2e3
Johnny Cage <thespammingbott@gmail.com>
2026-05-02 15:29:59 +0100

    import commentController

--- apps/api/routers/commentRouter.js
+++ apps/api/routers/commentRouter.js
@@ -1,7 +1,8 @@
 const { Router } = require("express")
 const commentRouter = Router()
+const commentController = require("../controllers/commentController")
 
-commentRouter.get("/posts/:id/comments", (req, res) => {})
+commentRouter.get("/posts/:id/comments", commentController.fetchAllcomments)
 commentRouter.post("/posts/:id/comments", (req, res) => {})
 commentRouter.delete("/posts/:id/comments/:cid", (req, res) => {})
 
