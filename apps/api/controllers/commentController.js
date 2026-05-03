10de2d216f830080512374c6103cb298e90758b3
Johnny Cage <thespammingbott@gmail.com>
2026-05-03 13:39:06 +0100

    feat: implement createComment function

--- apps/api/controllers/commentController.js
+++ apps/api/controllers/commentController.js
@@ -19,6 +19,26 @@ async function fetchAllcomments(req, res) {
     }
 }
 
+async function createComment(req, res) {
+    try {
+        const { content } = req.body
+        const postId = req.params.id
+        const authorId = req.user.id
+        const comment = await prisma.comment.create({
+            data: {
+                content,
+                postId,
+                authorId,
+            }
+        })
+        if (!content || postId || authorId) return res.status(400).json({ error: "Content, postId and authorId are all required!" })
+        res.json(comment)
+    } catch(err) {
+        res.status(500).json({ error: "Something went wrong" })
+    }
+}
+
 module.exports = {
     fetchAllcomments,
+    createComment
 }
--- apps/api/routers/commentRouter.js
+++ apps/api/routers/commentRouter.js
@@ -3,7 +3,7 @@ const commentRouter = Router()
 const commentController = require("../controllers/commentController")
 
 commentRouter.get("/posts/:id/comments", commentController.fetchAllcomments)
-commentRouter.post("/posts/:id/comments", (req, res) => {})
+commentRouter.post("/posts/:id/comments", commentController.createComment)
 commentRouter.delete("/posts/:id/comments/:cid", (req, res) => {})
 
 module.exports = commentRouter;
