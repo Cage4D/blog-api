ab70784802476b5d2767da7c45572534e848d595
Johnny Cage <thespammingbott@gmail.com>
2026-05-08 12:39:56 +0100

    fix typo in module.exports

--- apps/api/lib/prisma.js
+++ apps/api/lib/prisma.js
@@ -6,4 +6,4 @@ const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
-exports.module = { prisma };
+module.exports = { prisma };
