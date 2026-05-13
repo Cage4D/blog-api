c822953ecb5d973ee5edf54e4b1fa42f4db9f869
Johnny Cage <thespammingbott@gmail.com>
2026-05-13 19:08:52 +0100

    import authRouter, parse json inputs nad implement authRouter

--- apps/api/app.js
+++ apps/api/app.js
@@ -2,11 +2,14 @@ const express = require("express")
 const indexRouter = require("./routers/indexRouter")
 const postRouter = require("./routers/postRouter")
 const commentRouter = require("./routers/commentRouter")
+const authRouter = require("./routers/authRouter")
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use("/", indexRouter)
+app.use("/auth", authRouter)
 app.use("/posts/:id/comments", commentRouter)
 app.use("/posts", postRouter)
 
