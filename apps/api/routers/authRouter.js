bd0747d3e3a3ed829c5edc298741eb73b9bc5279
Johnny Cage <thespammingbott@gmail.com>
2026-05-13 19:07:01 +0100

    remove auth prefix from routes in authRouter

--- apps/api/routers/authRouter.js
+++ apps/api/routers/authRouter.js
@@ -2,8 +2,8 @@ const { Router } = require("express")
 const authRouter = Router()
 const authController = require("../controllers/authController")
 
-authRouter.post("/auth/signup", authController.signup)
-authRouter.post("/auth/login", authController.login)
-authRouter.post("/auth/logout", authController.logout)
+authRouter.post("/signup", authController.signup)
+authRouter.post("/login", authController.login)
+authRouter.post("/logout", authController.logout)
 
 module.exports = authRouter;
